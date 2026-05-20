f49b63d2ea31bcc52e23a7f4fd71a5834c83d096
Anwar Hossain <cseanwar@gmail.com>
2026-05-20 12:31:45 +0200

    Added edit a facility API

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const cors = require("cors")
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 dotenv.config()
 
 const uri = process.env.MONGODB_URI;
@@ -45,7 +45,16 @@ async function run() {
     // Facility Details page API
     app.get("/facilities/:id", async (req, res) => {
       const { id } = req.params;
-      const result = await destinationCollection.findOne({
+      const result = await booknplayCollection.findOne({
+        _id: new ObjectId(id),
+      });
+      res.json(result);
+    });
+
+    // Edit a facility API
+    app.delete("/facilities/:id", async (req, res) => {
+      const { id } = req.params;
+      const result = await booknplayCollection.deleteOne({
         _id: new ObjectId(id),
       });
       res.json(result);
